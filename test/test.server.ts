af17cbbe8ccb77ce81e6b189463e48491c24f215
uu-z <ncysatnaf@gmail.com>
2024-06-11 17:44:39 +0800

    udpate demo

--- test/test.server.ts
+++ test/test.server.ts
@@ -3,7 +3,6 @@ import { Hono } from 'hono'
 import { cors } from "hono/cors"
 import { URPC } from "../src/urpc";
 import { createServerClient } from '../src/client';
-import { applyPatch } from 'fast-json-patch';
 
 
 let data = {
@@ -13,23 +12,30 @@ let data = {
 };
 
 const func1 = URPC.Func({
-  input: { add_fruit: "banana", fruits: [] },
+  input: { add_fruit: "banana" },
   func: ({ input }) => data.enums.push(input.add_fruit),
   uiConfig: () => ({
-    fruits: {
+    add_fruit: {
       selectOptions: data.enums.map(i => ({ label: i, value: i }))
     }
   })
 })
 
-const collections = [{ name: "Data1" }, { name: "Data2" }]
-const collectionProxy = new Proxy(collections, {
+let collections = [{ name: "Data1" }, { name: "Data2" }]
+const collectionProxy = new Proxy(collections.map(i => ({ ...i, remove() { collections = collections.filter(t => t.name !== i.name) } })), {
   set(t, p: string, v) {
+    if (p == 'length') return Reflect.set(t, p, v)
     console.log(`set path ${p} value ${JSON.stringify(v, null, 2)}`)
+    //TODO upsert
+    if (!t[p]) {
+    } else {
+    }
     return Reflect.set(t, p, v)
   },
   deleteProperty(t, p: string) {
-    console.log(`delete path ${p}`)
+    const item = t[p]
+    console.log(`delete item ${JSON.stringify(item, null, 2)}`)
+    // TODO update db
     return Reflect.deleteProperty(t, p)
   }
 })
@@ -40,21 +46,19 @@ export const urpc = new URPC({
   schemas: {
     object: {
       func1,
-      object1: {
-        sum1: URPC.Func({
-          input: { a: 0, b: 0 },
-          func: ({ input }) => input.a + input.b,
-        }),
-        data1: URPC.Var({ get: () => data }),
-        collections: URPC.Var({
-          get: () => collectionProxy,
-          uiConfig: () => {
-            return {
-              name: {}
-            }
+      sum1: URPC.Func({
+        input: { a: 0, b: 0 },
+        func: ({ input }) => input.a + input.b,
+      }),
+      data1: URPC.Var({ get: () => data }),
+      collections: URPC.Var({
+        get: () => collectionProxy,
+        uiConfig: () => {
+          return {
+            name: {}
           }
-        }),
-      }
+        }
+      }),
     },
   },
 });
